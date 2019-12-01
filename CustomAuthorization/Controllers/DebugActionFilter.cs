22545e5e2bc75cc68498e0d375ac8b9bf4649d36
arbaaz360 <arbaaz9044@gmail.com>
2019-12-01 21:51:50 +0530

    initial commit

---
+++ CustomAuthorization/Controllers/DebugActionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace CustomAuthorization.Controllers
+{
+    public class CustomActionFilter :  ActionFilterAttribute
+    {
+        public int  InputParameter { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var x = this.InputParameter;
+
+            filterContext.Result = new JsonResult(new { HttpStatusCode.Unauthorized });
+        }
+    }
+}
